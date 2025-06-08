51d31190eb3bfefd5d85363e72517eb81986c4ad
zandocommando <xboyzander@gmail.com>
2025-06-08 08:17:35 -0700

    Add typography and separate fonts into own file

---
+++ app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Font.kt
@@ -0,0 +1,34 @@
+package com.mobiledevcampus.notemark.core.presentation.design_system
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.mobiledevcampus.notemark.R
+
+// Space Grotesk Font
+val SpaceGrotesk = FontFamily(
+    Font(
+        resId = R.font.spacegrotesk_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.spacegrotesk_bold,
+        weight = FontWeight.Bold
+    )
+)
+
+// Inter Font
+val Inter = FontFamily(
+    Font(
+        resId = R.font.inter_18pt_light,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.inter_18pt_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.inter_28pt_semibold,
+        weight = FontWeight.SemiBold
+    )
+)
--- app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Type.kt
+++ app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Type.kt
@@ -2,33 +2,55 @@ package com.mobiledevcampus.notemark.core.presentation.design_system
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    // Titles
+    displayLarge = TextStyle(
+        fontFamily = SpaceGrotesk,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = TextStyle(
+        fontFamily = SpaceGrotesk,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = SpaceGrotesk,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+
+    // Body
+    bodyLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
     )
-    */
 )
