df0b2e4ad7ac85a4d2da28b14961f192ea2dc20e
Andres Ruiz <andres-ruiz@sandhills.com>
2025-06-10 12:20:54 -0500

    add auth implementation

---
+++ app/src/main/java/com/mobiledevcampus/notemark/auth/data/AuthRepositoryImpl.kt
@@ -0,0 +1,48 @@
+import com.mobiledevcampus.notemark.auth.data.LoginRequest
+import com.mobiledevcampus.notemark.auth.data.LoginResponse
+import com.mobiledevcampus.notemark.auth.data.RegisterRequest
+import com.mobiledevcampus.notemark.auth.domain.AuthRepository
+import com.mobiledevcampus.notemark.core.data.networking.post
+import com.mobiledevcampus.notemark.core.domain.AuthInfo
+import com.mobiledevcampus.notemark.core.domain.SessionStorage
+import com.mobiledevcampus.notemark.core.domain.util.DataError
+import com.mobiledevcampus.notemark.core.domain.util.EmptyResult
+import com.mobiledevcampus.notemark.core.domain.util.asEmptyDataResult
+import io.ktor.client.HttpClient
+import com.mobiledevcampus.notemark.core.domain.util.Result
+
+class AuthRepositoryImpl(
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
+): AuthRepository {
+
+    override suspend fun login(username: String, email: String, password: String): EmptyResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, LoginResponse>(
+            route = "/login",
+            body = LoginRequest(
+                username = username,
+                email = email,
+                password = password
+            )
+        )
+        if(result is Result.Success) {
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken,
+                )
+            )
+        }
+        return result.asEmptyDataResult()
+    }
+
+    override suspend fun register(email: String, password: String): EmptyResult<DataError.Network> {
+        return httpClient.post<RegisterRequest, Unit>(
+            route = "/register",
+            body = RegisterRequest(
+                email = email,
+                password = password
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/mobiledevcampus/notemark/core/data/networking/HttpClientFactory.kt
@@ -0,0 +1,89 @@
+package com.mobiledevcampus.notemark.core.data.networking
+
+import android.util.Log
+import com.mobiledevcampus.notemark.BuildConfig
+import com.mobiledevcampus.notemark.core.domain.AuthInfo
+import com.mobiledevcampus.notemark.core.domain.SessionStorage
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import com.mobiledevcampus.notemark.core.domain.util.Result
+
+class HttpClientFactory(
+     private val sessionStorage: SessionStorage
+) {
+    fun build(): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.d("### API REQUEST ###", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                 header("X-User-Email", BuildConfig.CAMPUS_EMAIL)
+            }
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                         val info = sessionStorage.get()
+                        BearerTokens(
+                            accessToken = info?.accessToken ?: "",
+                            refreshToken = info?.refreshToken ?: ""
+                        )
+                    }
+                    refreshTokens {
+                        val info = sessionStorage.get()
+                        val response = client.post<AccessTokenRequest, AccessTokenResponse>(
+                            route = "/accessToken",
+                            body = AccessTokenRequest(
+                                refreshToken = info?.refreshToken ?: "",
+                            )
+                        )
+                        if(response is Result.Success) {
+                            val newAuthInfo = AuthInfo(
+                                accessToken = response.data.accessToken,
+                                refreshToken = info?.refreshToken ?: "",
+                            )
+                            sessionStorage.set(newAuthInfo)
+
+                            BearerTokens(
+                                accessToken = newAuthInfo.accessToken,
+                                refreshToken = newAuthInfo.refreshToken
+                            )
+                        } else {
+                            BearerTokens(
+                                accessToken = "",
+                                refreshToken = ""
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
