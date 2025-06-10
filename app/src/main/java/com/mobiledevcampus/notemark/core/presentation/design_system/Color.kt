bf9562c9bdd99a434ed40699c4b162e8ea6a3ff1
Andres Ruiz <andres-ruiz@sandhills.com>
2025-06-10 13:07:38 -0500

    update colors

--- app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Color.kt
+++ app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Color.kt
@@ -2,18 +2,10 @@ package com.mobiledevcampus.notemark.core.presentation.design_system
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Scheme
 val Surface = Color(0xFF1B1B1C)
 val OnSurface = Color(0xFF535364)
-val OnSurfaceVariant = Color(0xFFEFEFF2)
+val OnSurfaceVar = Color(0xFFEFEFF2)
 val Error = Color(0xFFE1294B)
 
 // Surface + Opacity
--- app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Theme.kt
+++ app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/Theme.kt
@@ -1,64 +1,30 @@
 package com.mobiledevcampus.notemark.core.presentation.design_system
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+val ColorScheme.OnSurfaceVariant: Color
+    get() = OnSurfaceVar
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 val AppColorScheme = lightColorScheme(
     primary = Primary,
     onPrimary = OnPrimary,
     surface = Surface,
     onSurface = OnSurface,
-    error = Error
+    error = Error,
 )
 
 @Composable
 fun NoteMarkTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = AppColorScheme,
         typography = Typography,
         content = content
     )
