ceea555d33a4818feca2623d264940608649baa2
zandocommando <xboyzander@gmail.com>
2025-06-12 18:16:16 -0700

    Text field implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.animation.core.lint)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -55,6 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.compose.material.icons.extended)
 
     // Splash Screen
     implementation(libs.androidx.core.splashscreen)
--- app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/components/NoteMarkTextField.kt
+++ app/src/main/java/com/mobiledevcampus/notemark/core/presentation/design_system/components/NoteMarkTextField.kt
@@ -1,2 +1,42 @@
 package com.mobiledevcampus.notemark.core.presentation.design_system.components
 
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.*
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+
+
+@Composable
+fun NoteMarkTextField (
+    title: String,
+    hintText: String,
+    text: String,
+    onTextChanged: (String) -> Unit,
+    isPassword: Boolean = false,
+    modifier: Modifier = Modifier
+) {
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = text,
+        onValueChange = onTextChanged,
+        label = { Text(title) },
+        placeholder = { Text(hintText) },
+        visualTransformation = if (isPassword && !passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = if (isPassword) KeyboardOptions(keyboardType = KeyboardType.Password) else KeyboardOptions.Default,
+        trailingIcon = {
+            if (isPassword) {
+                val icon = if (passwordVisible) Icons.Filled.Visibility else Icons.Filled.VisibilityOff
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = icon, contentDescription = "Toggle password visibility")
+                }
+            }
+        },
+        singleLine = true
+    )
+}
